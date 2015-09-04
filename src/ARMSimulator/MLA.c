692f69909a3b8eb7ab3492a36ae7739402ce6c76
darklord1310 <jet_wong@hotmail.com>
2015-09-04 20:15:42 +0800

    Completed MULT2, MLAT1 and MLST1

--- src/ARMSimulator/MLA.c
+++ src/ARMSimulator/MLA.c
@@ -10,52 +10,41 @@
 #include <stdio.h>
 
 
-/*Add with Carry Register To Register Encoding T1
-  ADCS        <Rdn>,<Rm>     Outside IT block.
-  ADC<c>      <Rdn>,<Rm>     Inside IT block.
+/*Multiply Accumulate Encoding T1
+    MLA<c> <Rd>,<Rn>,<Rm>,<Ra>
       
    31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
-  |0   1  0  0  0  0| 0  1  0  1|   Rm   |   Rdn  |             unused                  |
+  |1   1  1  1  1| 0  1  1  0| 0  0  0|     Rn    |     Ra    |    Rd   |0 0 0 0|   Rm  |
 
 where:
-          S           If present, specifies that the instruction updates the flags. Otherwise, the instruction does not
-                      update the flags.
-                      
-          <c><q>      See Standard assembler syntax fields on page A6-7.
-          
-          <Rd>        Specifies the destination register. If <Rd> is omitted, this register is the same as <Rn> and
-                      encoding T2 is preferred to encoding T1 if both are available (this can only happen inside
-                      an IT block). If <Rd> is specified, encoding T1 is preferred to encoding T2.
-                      
-          <Rn>        Specifies the register that contains the first operand. If the SP is specified for <Rn>, see ADD
-                      (SP plus register) on page A6-28.
-                      
-          <Rm>        Specifies the register that is optionally shifted and used as the second operand.
-          
-          <shift>     Specifies the shift to apply to the value read from <Rm>. If <shift> is omitted, no shift is
-                      applied and all encodings are permitted. If <shift> is specified, only encoding T3 is
-                      permitted. The possible shifts and how they are encoded are described in Shifts applied to a
-                      register on page A6-12.
-                      
-          Inside an IT block, if ADD<c> <Rd>,<Rn>,<Rd> cannot be assembled using encoding T1, it is assembled using
-          encoding T2 as though ADD<c> <Rd>,<Rn> had been written. To prevent this happening, use the .W qualifier.
+        <c><q>        See Standard assembler syntax fields on page A6-7.
+        
+        <Rd>          Specifies the destination register.
+        
+        <Rn>          Specifies the register that contains the first operand.
+        
+        <Rm>          Specifies the register that contains the second operand.
+        
+        <Ra>          Specifies the register containing the accumulate value.
 */
 void MLAT1(uint32_t instruction)
 {
-  uint32_t Rm =  getBits(instruction,21,19);
-  uint32_t Rdn = getBits(instruction,18,16);
-
- if(inITBlock())
- {
+  uint32_t Rm = getBits(instruction,3,0);
+  uint32_t Rd = getBits(instruction,11,8);
+  uint32_t Rn = getBits(instruction,19,16);
+  uint32_t Ra = getBits(instruction,15,12);
+  
+  if(inITBlock())
+  {
     if( checkCondition(cond) )
-      //executeADCRegister(Rdn, Rdn, Rm, 0);
+      coreReg[Rd] = coreReg[Rn] * coreReg[Rm] + coreReg[Ra];
 
     shiftITState();
- }
- else
-    //executeADCRegister(Rdn, Rdn, Rm, 1);
-  
-  coreReg[PC] += 2;
+  }
+  else
+    coreReg[Rd] = coreReg[Rn] * coreReg[Rm] + coreReg[Ra];
+
+  coreReg[PC] += 4;
 }
 
 
--- src/ARMSimulator/MULRegister.c
+++ src/ARMSimulator/MULRegister.c
@@ -52,23 +52,46 @@ void MULRegisterT1(uint32_t instruction)
 
 
 
+
+/*  
+  Multiply Register to Register Encoding T2
+  
+      MUL<c> <Rd>,<Rn>,<Rm>
+        
+   31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
+  |1   1  1  1  1| 0  1  1  0| 0  0  0|     Rn    | 1  1  1  1|    Rd   |0 0 0 0|   Rm  |
+    
+  where:
+            S           If present, specifies that the instruction updates the flags. Otherwise, the instruction does not
+                        update the flags.
+                        
+            <c><q>      See Standard assembler syntax fields on page A6-7.
+            
+            <Rd>        Specifies the destination register. If <Rd> is omitted, this register is the same as <Rn>.
+            
+            <Rn>        Specifies the register that contains the first operand.
+            
+            <Rm>        Specifies the register that contains the second operand.
+  
+*/
 void MULRegisterT2(uint32_t instruction)
 {
-  uint32_t Rn = getBits(instruction, 21, 19);
-  uint32_t Rdm = getBits(instruction, 18, 16);
+  uint32_t Rn = getBits(instruction, 19, 16);
+  uint32_t Rm = getBits(instruction, 3, 0);
+  uint32_t Rd = getBits(instruction, 11, 8);
 	
   if( inITBlock() )
   {  
     if( checkCondition(cond) )
-      executeMULRegister(Rdm, Rdm, Rn,0);  //status flag is not affected
+      executeMULRegister(Rm, Rd, Rn,0);  
     
     shiftITState();
   }
   else
-    executeMULRegister(Rdm, Rdm, Rn, 1);   //status flag is affected
+    executeMULRegister(Rm, Rd, Rn, 0);   
   
   
-  coreReg[PC] += 2;
+  coreReg[PC] += 4;
 }
 
 
@@ -83,7 +106,6 @@ void MULRegisterT2(uint32_t instruction)
 */
 void executeMULRegister(uint32_t Rm, uint32_t Rd, uint32_t Rn, uint32_t S)
 {
-  uint64_t answer = coreReg[Rm] * coreReg[Rn];
   coreReg[Rd] = coreReg[Rm] * coreReg[Rn];
   
   if(S == 1)
