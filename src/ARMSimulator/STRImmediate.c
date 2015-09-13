543b88bace4e97501140ac08c775bce91916a195
darklord1310 <jet_wong@hotmail.com>
2015-09-13 19:29:42 +0800

    Updated the comments for STRImmediateT2 and T3

--- src/ARMSimulator/STRImmediate.c
+++ src/ARMSimulator/STRImmediate.c
@@ -143,6 +143,31 @@ void STRBImmediateT1(uint32_t instruction)
 
 
 
+/*Store Register Byte (immediate) Encoding T2
+ * 
+    STRB<c>.W <Rt>,[<Rn>,#<imm12>]
+      
+   31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
+  | 1  1  1  1  1| 0  0| 0  1  0  0  0|     Rn    |     Rt    |          imm12          |
+
+  where:
+            <c><q>            See Standard assembler syntax fields on page A6-7.
+            
+            <Rt>              Specifies the source register. This register is allowed to be the SP.
+            
+            <Rn>              Specifies the base register. This register is allowed to be the SP.
+            
+            +/-               Is + or omitted to indicate that the immediate offset is added to the base register value
+                              (add == TRUE), or – to indicate that the offset is to be subtracted (add == FALSE). Different
+                              instructions are generated for #0 and #-0.
+                              
+            <imm>             Specifies the immediate offset added to or subtracted from the value of <Rn> to form the
+                              address. Allowed values are multiples of 4 in the range 0-124 for encoding T1, multiples of
+                              4 in the range 0-1020 for encoding T2, any value in the range 0-4095 for encoding T3, and
+                              any value in the range 0-255 for encoding T4. For the offset addressing syntax, <imm> can be
+                              omitted, meaning an offset of 0.
+          
+*/
 void STRBImmediateT2(uint32_t instruction)
 {
   
@@ -150,6 +175,33 @@ void STRBImmediateT2(uint32_t instruction)
 }
 
 
+/*Store Register Byte (immediate) Encoding T3
+ * 
+    STRB<c> <Rt>,[<Rn>,#-<imm8>]
+    STRB<c> <Rt>,[<Rn>],#+/-<imm8>
+    STRB<c> <Rt>,[<Rn>,#+/-<imm8>]!
+      
+   31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
+  | 1  1  1  1  1| 0  0| 0  0  0  0  0|     Rn    |     Rt    | 1| P|U|W|     imm8      |
+
+  where:
+            <c><q>            See Standard assembler syntax fields on page A6-7.
+            
+            <Rt>              Specifies the source register. This register is allowed to be the SP.
+            
+            <Rn>              Specifies the base register. This register is allowed to be the SP.
+            
+            +/-               Is + or omitted to indicate that the immediate offset is added to the base register value
+                              (add == TRUE), or – to indicate that the offset is to be subtracted (add == FALSE). Different
+                              instructions are generated for #0 and #-0.
+                              
+            <imm>             Specifies the immediate offset added to or subtracted from the value of <Rn> to form the
+                              address. Allowed values are multiples of 4 in the range 0-124 for encoding T1, multiples of
+                              4 in the range 0-1020 for encoding T2, any value in the range 0-4095 for encoding T3, and
+                              any value in the range 0-255 for encoding T4. For the offset addressing syntax, <imm> can be
+                              omitted, meaning an offset of 0.
+          
+*/
 void STRBImmediateT3(uint32_t instruction)
 {
   
